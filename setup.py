29129c3383d87d55d20d75049e93c0aae7763361
John Song <songq@otpp.com>
2020-04-05 21:37:19 -0400

    Linear Kalman Filter Completed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='trackingfilters',
+      version='0.1',
+      description='Collection of Tracking Filters Commonly Used in Signal Processing',
+      url='http://github.com/storborg/funniest',
+      author='johnsoong216',
+      author_email='johnsoong216@hotmail.com',
+      license='MIT',
+      packages=['trackingfilters'],
+      zip_safe=False)
---
+++ trackingfilters/exceptions.py
@@ -0,0 +1,2 @@
+class LinKalmanException(Exception):
+    pass
