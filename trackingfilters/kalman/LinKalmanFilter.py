80a5de6dceb720a79bb81365869698a87982d9e8
John Song <songq@otpp.com>
2020-04-09 18:10:27 -0400

    add

--- trackingfilters/kalman/LinKalmanFilter.py
+++ trackingfilters/kalman/LinKalmanFilter.py
@@ -304,5 +304,91 @@ class LinKalmanFilter:
         return measure_vec
 
 
+import backtrader as bt
+import datetime
 
+class KalmanPairTrading(bt.Strategy):
+    params = dict(
+        enter=[1, 3, 4],  # data ids are 1 based
+        hold=[7, 10, 15],  # data ids are 1 based
+        usebracket=True,
+        rawbracket=True,
+        pentry=0.015,
+        plimits=0.03,
+        valid=10,
+    )
 
+    def notify_order(self, order):
+        if order.status == order.Submitted:
+            return
+
+        dt, dn = self.datetime.date(), order.data._name
+        print('{} {} Order {} Status {}'.format(
+            dt, dn, order.ref, order.getstatusname())
+        )
+
+        whichord = ['main', 'stop', 'limit', 'close']
+        if not order.alive():  # not alive - nullify
+            dorders = self.o[order.data]
+            idx = dorders.index(order)
+            dorders[idx] = None
+            print('-- No longer alive {} Ref'.format(whichord[idx]))
+
+            if all(x is None for x in dorders):
+                dorders[:] = []  # empty list - New orders allowed
+
+    def __init__(self):
+        self.o = dict()  # orders per data (main, stop, limit, manual-close)
+        self.holding = dict()  # holding periods per data
+
+    def next(self):
+        for i, d in enumerate(self.datas):
+            dt, dn = self.datetime.date(), d._name
+            pos = self.getposition(d).size
+            print('{} {} Position {}'.format(dt, dn, pos))
+
+            if not pos and not self.o.get(d, None):  # no market / no orders
+                if dt.weekday() == self.p.enter[i]:
+                    if not self.p.usebracket:
+                        self.o[d] = [self.buy(data=d)]
+                        print('{} {} Buy {}'.format(dt, dn, self.o[d][0].ref))
+
+                    else:
+                        p = d.close[0] * (1.0 - self.p.pentry)
+                        pstp = p * (1.0 - self.p.plimits)
+                        plmt = p * (1.0 + self.p.plimits)
+                        valid = datetime.timedelta(self.p.valid)
+
+                        if self.p.rawbracket:
+                            o1 = self.buy(data=d, exectype=bt.Order.Limit,
+                                          price=p, valid=valid, transmit=False)
+
+                            o2 = self.sell(data=d, exectype=bt.Order.Stop,
+                                           price=pstp, size=o1.size,
+                                           transmit=False, parent=o1)
+
+                            o3 = self.sell(data=d, exectype=bt.Order.Limit,
+                                           price=plmt, size=o1.size,
+                                           transmit=True, parent=o1)
+
+                            self.o[d] = [o1, o2, o3]
+
+                        else:
+                            self.o[d] = self.buy_bracket(
+                                data=d, price=p, stopprice=pstp,
+                                limitprice=plmt, oargs=dict(valid=valid))
+
+                        print('{} {} Main {} Stp {} Lmt {}'.format(
+                            dt, dn, *(x.ref for x in self.o[d])))
+
+                    self.holding[d] = 0
+
+            elif pos:  # exiting can also happen after a number of days
+                self.holding[d] += 1
+                if self.holding[d] >= self.p.hold[i]:
+                    o = self.close(data=d)
+                    self.o[d].append(o)  # manual order to list of orders
+                    print('{} {} Manual Close {}'.format(dt, dn, o.ref))
+                    if self.p.usebracket:
+                        self.cancel(self.o[d][1])  # cancel stop side
+                        print('{} {} Cancel {}'.format(dt, dn, self.o[d][1]))
